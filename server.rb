7ae92261902ae66b0c8ddd2c691358277e93d0a9
yamamori <yamamori@sa.cs.titech.ac.jp>
2014-05-27 20:38:03 +0900

    Implemented the function getting the contents of google.com
    But it doesn't track the page's redirect (HTTP 302 response). Is it enough for task No.4?

--- server.rb
+++ server.rb
@@ -1,12 +1,20 @@
 #! /usr/bin/env ruby -Ku
 
+require 'net/http'
+require 'uri'
+
 
 class Server
   def sayHello()
 	print "Hello World\n"
   end
 
+  def showGoogle()
+    contents = Net::HTTP.get URI.parse('http://www.google.com/')
+    puts contents
+  end
+
 end
 
 s = Server.new()
-s.sayHello()
+s.showGoogle()
